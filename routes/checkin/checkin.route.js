3650f8bf01a6945ab9b9fcaf401f10662c87088f
O'Dell Obrien <dashhax@gmail.com>
2021-01-13 10:34:22 +0800

    Checkin

--- routes/checkin/checkin.route.js
+++ routes/checkin/checkin.route.js
@@ -48,24 +48,38 @@ route.post("/submit", async (req, res) => {
         } = req.body;
 
         let tokenPayload = jwt.verify(token, config.JWT.accessKey);
-        if (tokenPayload.puiPIN != id) return err(res, { error: "Invalid token!"});
+        if (tokenPayload.puiPIN != id) return err(res, { error: "Invalid token!", l: 1});
 
-        let search = await db("pui_lists").innerJoin("locations", "locations.pui_id", "pui_lists.id").where({ login_id: id }).select("*").limit(1);
-        if (search.length == 0) return err(res, { error: "Invalid token! "});
+        let search = await db("pui_lists").innerJoin("locations", "locations.pui_id", "pui_lists.id").where({ login_id: id })
+                            .select("pui_lists.id AS pui_ID", 
+                                    "locations.lat AS lat2", "locations.long", "locations.radius").limit(1);
+        if (search.length == 0) return err(res, { error: "Invalid token! ", l: 2});
 
         let pui = search[0];
-        console.log(pui);
+        console.log(pui, tokenPayload);
 
-        if (pui.id != tokenPayload.puiID) return err(res, {error : "Invalid token!"});
+        if (pui.pui_ID != tokenPayload.puiID) return err(res, {error : "Invalid token!", l: 3});
 
         let dist = faces.findDistance(tokenPayload.puiID, face);
         if (dist < config.Infer.Threshold) return err(res, { error: "Identity mismatch! Please try again!" });
 
-        let { acc, lat1, lng } = location;
+        let { acc, lat: lat1, lng } = location;
         let { lat2, long, radius } = pui;
 
-        let isBounded = insideCircle({ lat: lat1, lon: lng }, { lat: lat2, lon: long }, radius + (acc / 2));
+        let isBounded = insideCircle({ lat: parseFloat(lat1), lon: parseFloat(lng) }, { lat: parseFloat(lat2), lon: parseFloat(long) }, radius + (acc / 2));
         
+        await db("checkins").insert({
+            pui: pui.pui_ID,
+            checked_time: new Date(),
+            lat: lat1,
+            long: lng,
+            inside: isBounded ? 1 : 0
+        });
+
+        tokenPayload.rnd = Math.round(Math.random() * Number.MAX_SAFE_INTEGER);
+        let newToken = jwt.sign(tokenPayload, config.JWT.accessKey);
+
+        return res.json({ status: "success", token: newToken });
 
     } catch (error) {
         return err(res, { error: error.message });

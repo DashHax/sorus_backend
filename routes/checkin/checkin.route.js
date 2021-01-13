5ab8c4c90eb0169373b63bb8340fb0c9ef16387c
O'Dell Obrien <dashhax@gmail.com>
2021-01-13 10:21:13 +0800

    Update

--- routes/checkin/checkin.route.js
+++ routes/checkin/checkin.route.js
@@ -6,6 +6,8 @@ const { err } = require("../helper");
 const config = require("../../config");
 const jwt = require("jsonwebtoken");
 
+const { insideCircle } = require("geolocation-utils");
+
 const route = express.Router();
 
 route.post("/login", async (req, res) => {
@@ -23,7 +25,7 @@ route.post("/login", async (req, res) => {
             console.log(`${userID} login at ${dist} similarity`)
 
             let payload = { puiID: userID, puiPIN: pin, rnd: Math.round(Math.random() * Number.MAX_SAFE_INTEGER) }
-            let token = jwt.sign(payload, config.JWT.accessKey, { expiresIn: "24h" });
+            let token = jwt.sign(payload, config.JWT.accessKey);
 
             return res.json({ status: "success", token, user: {  id: user.id, name: user.fullname }});
         } else {
@@ -33,7 +35,42 @@ route.post("/login", async (req, res) => {
     } catch (error) {
         return err(res, { error: error.message });
     }
-})
+});
+
+route.post("/submit", async (req, res) => {
+    try {
+        let {
+            id,//: localStorage["login_pin"],
+            token,//: localStorage["token"],
+            location,//: location,
+            histograms,//: { left: l_hist, right: r_hist },
+            face,//: snappedFace.embedding
+        } = req.body;
+
+        let tokenPayload = jwt.verify(token, config.JWT.accessKey);
+        if (tokenPayload.puiPIN != id) return err(res, { error: "Invalid token!"});
+
+        let search = await db("pui_lists").innerJoin("locations", "locations.pui_id", "pui_lists.id").where({ login_id: id }).select("*").limit(1);
+        if (search.length == 0) return err(res, { error: "Invalid token! "});
+
+        let pui = search[0];
+        console.log(pui);
+
+        if (pui.id != tokenPayload.puiID) return err(res, {error : "Invalid token!"});
+
+        let dist = faces.findDistance(tokenPayload.puiID, face);
+        if (dist < config.Infer.Threshold) return err(res, { error: "Identity mismatch! Please try again!" });
+
+        let { acc, lat1, lng } = location;
+        let { lat2, long, radius } = pui;
+
+        let isBounded = insideCircle({ lat: lat1, lon: lng }, { lat: lat2, lon: long }, radius + (acc / 2));
+        
+
+    } catch (error) {
+        return err(res, { error: error.message });
+    }
+});
 
 module.exports = {
     path: '/checkin',
